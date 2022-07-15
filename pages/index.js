fc0e877c368f23da84a6771766cf22b7a821492b
snowiewdev <snowiewongdev@gmail.com>
2022-07-15 16:28:46 -0700

    init next

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title> Web Dev News</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+
+      <h1>Hello Next :)</h1>
+    </div>
+  );
+}
